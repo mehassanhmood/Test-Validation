bffd08da7216bdc46ce81c72cf35359f485dc362
Hassan Mehmood <mehassanhmood@gmail.com>
2024-01-13 06:54:47 -0500

    Local test passed

--- write_your_name.py
+++ write_your_name.py
@@ -1,2 +1,11 @@
+"""
+Module: write_your_name
+
+This module defines a function to get the author's name.
+"""
+
 def hi_my_name_is():
+    """
+    This function returns the author's name.
+    """
     return "Hassan Mehmood"
